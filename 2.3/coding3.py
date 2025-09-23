ed1106ceb63806e46d7840b34e0aecfbc054d0e3
a-gogna123 <gognaaaryan1@gmail.com>
2025-09-23 21:49:40 +0000

    exercise 3

--- 2.3/coding3.py
+++ 2.3/coding3.py
@@ -25,3 +25,30 @@ Would you like fries for $3? (Yes/No)
 > yes
 Your total is $3.42
 """
+
+#McDonald's Program
+#Aaryan Gogna
+#Sep 23, 2025
+
+print("Would you like a burger for $5?")
+
+response_1 = input().lower()
+
+print("Would you like fries for $3? ")
+
+response_2 = input().lower()
+
+if response_1 == "yes" and response_2 == "yes":
+    print("Your total is $9.12.")
+    
+elif response_1 == "yes" and response_2 == "no":
+    print("Your total is $5.70.")
+    
+elif response_1 == "no" and response_2 == "yes":
+    print("Your total is $3.42.")
+    
+elif response_1 == "no" and response_2 == "no":
+    print("Have a nice day, your total is $0.")
+    
+else:
+    print("Sorry, I don't understand what you said.")
