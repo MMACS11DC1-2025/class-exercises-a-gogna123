d6a9f92836460532cd412270113f5daad67c3d1b
a-gogna123 <gognaaaryan1@gmail.com>
2025-12-08 01:38:02 +0000

    Day 2  Committing Changes! View the day 2 report in the README.md file.

--- 6.7/README.md
+++ 6.7/README.md
@@ -10,4 +10,18 @@ Today I chose my project theme: analyzing fire images to estimate the size of a 
 2. Create if-statements to label each fire as Small, Medium, Large, or Extreme
 
 # Day 2:
+Today I did small singular steps to get closer and closer to my final result which will display a statistic. I also included a time function to determine the amount of time the program took to process each image.
+1. On day 1, I was just displaying the percents of the fire and orange pixels. Now, I made a new variable called "fire_percent"
+which added all the white and orange pixels together.
+2. I then made conditional statements, based on the percentage of the fire, I asked CHATGPT what would be a good observation based on my outputs.
+3. I also added a time function that lets the user know the time it takes for each image thats processed.
+4. Overall, day 2 was a success.
+
+# Day 3 Plan:
+1. Create a list that stores each images name and percent score used the append feature.
+2. Print the list for testing.
+
+# Day 3:
+
+
 
--- 6.7/main.py
+++ 6.7/main.py
@@ -1,32 +1,35 @@
 # Aaryan Gogna
-# Novemeber 25, 2025
+# November 25, 2025
 # Image Explorer Project
 
+import time
 from PIL import Image
 
 def is_orange(r, g, b):
-    return (r > 170 and g > 80 and r > g and b < 120)
+    return (r >= 180 and 80 <= g <= 215 and b <= 130 and r > g)
 
 def is_white(r, g, b):
-    return (r > 220 and g > 210 and b > 150)
+    return (r >= 210 and g >= 210 and b >= 180)
 
 
 image_files = [
-    "6.7/Images/BonFire.jpg",
-    "6.7/Images/BurningMatch.jpg",
-    "6.7/Images/BushFire.jpg",
-    "6.7/Images/CampFire.jpg",
-    "6.7/Images/CandleFlame.jpg",
-    "6.7/Images/FireExplosion.jpg",
-    "6.7/Images/FirePlace.jpg",
-    "6.7/Images/ForestFire.jpg",
-    "6.7/Images/HouseFire.jpg",
-    "6.7/Images/WildFireSmoke.jpg"
+    "BonFire",
+    "BurningMatch",
+    "BushFire",
+    "CampFire",
+    "CandleFlame",
+    "FireExplosion",
+    "FirePlace",
+    "ForestFire",
+    "HouseFire",
+    "WildFireSmoke"
 ]
 
 for filename in image_files:
 
-    file = Image.open(filename)
+    start_time = time.time()
+
+    file = Image.open("6.7/Images/" + filename + ".jpg")
     img = file.load()
 
     width = file.width
@@ -50,8 +53,23 @@ for filename in image_files:
     total_pixels = width * height
     orange_percent = 100 * orange_pixels / total_pixels
     white_percent = 100 * white_pixels / total_pixels
+    fire_percent = int(white_percent) + int(orange_percent)
 
-    print("Orange: {:.3f}%".format(orange_percent))
-    print("White: {:.3f}%".format(white_percent))
+    if fire_percent < 6:
+        size = "SMALL Fire. Proceed with caution!"
+    elif fire_percent < 16:
+        size = "MEDIUM Fire. Don't approach without professionals!"
+    elif fire_percent < 31:
+        size = "LARGE Fire. Get yourself out of that place!"
+    else:
+        size = "EXTREME Fire. Stay OUT!"
+
+    
+    end_time = time.time()
+    elapsed = end_time - start_time
+
+    print(filename + ":")
+    print(size)
+    print("Processing Time: {:.3f} seconds".format(elapsed))
     print()  
 
